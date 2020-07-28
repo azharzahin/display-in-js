e9ed2e55591c7344633cd7a8c130e595892316eb
azharzahin <18713606+azharzahin@users.noreply.github.com>
2020-07-28 18:54:51 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// it is smart programming within 15minute i created this...fuck off
+
+var d = new Date();
+
+
+alert("CURENT DATE AND TIME IS: " + d);
+confirm("R U READY TO GO AHEAD");
+prompt("WRITE YOUR BEST FRIEND NAME, and email-id and mobile number DOWN");
+alert("THANKS FOR GIVING THE INFORMATION YOU FOOL");
